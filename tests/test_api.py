cc73993f335d9bded695ea32d8d6367a67cd9480
Othmane ZENZAMY <ozenzamy@iliad-free.fr>
2025-01-11 03:52:07 +0100

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import requests
 
 def test_api_healthcheck():
-    response = requests.get("http://127.0.0.1:8000/")
+    response = requests.get("https://ocproject.onrender.com/")
     assert response.status_code == 200
     assert response.json() == 'Welcome to the API'
