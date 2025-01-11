e4d4a87070706da9dc3fa3a5822816858aec5c2e
Othmane ZENZAMY <ozenzamy@iliad-free.fr>
2025-01-11 03:33:26 +0100

    updating script add feature_names.pkl

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ def test_api_healthcheck():
     assert response.json() == 'Welcome to the API'  # Use json() to parse the response correctly
 
 def test_prediction_endpoint():
-    client_id = 192535  # Replace with a valid client_id from your test dataset
+    client_id = 345437  # Replace with a valid client_id from your test dataset
     response = requests.get(f"http://127.0.0.1:8000/prediction/{client_id}")
     assert response.status_code == 200
     response_data = response.json()
@@ -16,7 +16,7 @@ def test_prediction_endpoint():
     assert isinstance(response_data["prediction"], int)
 
 def test_check_client_id():
-    client_id = 192535  # Replace with a valid client_id from your test dataset
+    client_id = 345437  # Replace with a valid client_id from your test dataset
     response = requests.get(f"http://127.0.0.1:8000/{client_id}")
     assert response.status_code == 200
     response_data = response.json()
