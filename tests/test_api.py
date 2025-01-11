bef1499fc90c6a5acde64c919a44e98c53173e12
Othmane ZENZAMY <ozenzamy@iliad-free.fr>
2025-01-11 03:36:57 +0100

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,13 @@
 import requests
 
 def test_api_healthcheck():
-    response = requests.get("http://127.0.0.1:8000/")
+    response = requests.get("http://0.0.0.0:8000/")
     assert response.status_code == 200
     assert response.json() == 'Welcome to the API'  # Use json() to parse the response correctly
 
 def test_prediction_endpoint():
     client_id = 345437  # Replace with a valid client_id from your test dataset
-    response = requests.get(f"http://127.0.0.1:8000/prediction/{client_id}")
+    response = requests.get(f"http://0.0.0.0:8000/prediction/{client_id}")
     assert response.status_code == 200
     response_data = response.json()
     assert "probability" in response_data
@@ -17,7 +17,7 @@ def test_prediction_endpoint():
 
 def test_check_client_id():
     client_id = 345437  # Replace with a valid client_id from your test dataset
-    response = requests.get(f"http://127.0.0.1:8000/{client_id}")
+    response = requests.get(f"http://0.0.0.0:8000/{client_id}")
     assert response.status_code == 200
     response_data = response.json()
     assert response_data in [True, False]  # The response should be either True or False
